1002c5e75e3c123869d75c5db75fc9ee1617c858
CodeGuy0 <pranaykumarjha989@gmail.com>
2024-09-02 13:14:10 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -19,9 +32,12 @@ let getRandomUser = () => {
 };
 
 
-//INSERTING NEW DATA
 
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+//....................FROM HERE..................................
+// //INSERTING NEW DATA
+
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
 // let user = ["123", "123_newuser", "abc@gmail.com", "abc" ];
 
@@ -32,21 +48,113 @@ let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
 // INSERT IN BULK
 
-let data = [];
-for (let i=1; i<=100; i++){
-    data.push(getRandomUser()); // 100 fake users
-}
+// let data = [];
+// for (let i=1; i<=100; i++){
+//     data.push(getRandomUser()); // 100 fake users
+// }
+// ....... Till Here we have used this code for random data so we comment this.............
 
-try {
-    connection.query(q, [data], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    })
-} catch (err) {
-    console.log(err);
-}
 
-connection.end();
+
+// HOME ROUTE :...........................................
+
+app.get("/", (req, res)=>{
+    let q = `SELECT count(*) FROM user`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let count = result[0]["count(*)"];
+            res.render("home.ejs", { count });
+        });
+     } catch (err) {
+        console.log(err);
+        res.send("some error in DB");
+     }
+    });
+
+
+    // Show Route....................................................
+
+app.get("/user", (req, res) => {
+    let q = `SELECT * FROM user`;
+    try {
+        connection.query(q, (err, users) => {
+            if (err) throw err;
+           res.render("showusers.ejs", {users});
+        });
+     } catch (err) {
+        console.log(err);
+        res.send("some error in DB");
+     }
+});
+
+// EDIT ROUTE.......................................................
+
+app.get("/user/:id/edit", (req, res)=>{
+    let { id } = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", {user});
+        });
+     } catch (err) {
+        console.log(err);
+        res.send("some error in DB");
+     }
+});
+
+// UPDATE (DB) Route..................................................
+app.patch("/user/:id", (req, res) => {
+    let{ id } = req.params;
+    let {password: formPass, username: newUsername} = req.body;
+    let q = `SELECT * FROM user WHERE id = '${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if (formPass != user.password){
+                res.send("WRONG password");
+            } else {
+                let q2 = `UPDATE user SET username='${newUsername}' WHERE id = '${id}'`;
+                connection.query(q2, (err, result) => {
+                    if (err) throw err;
+                    res.redirect("/user");
+                });
+            }
+        });
+     } catch (err) {
+        console.log(err);
+        res.send("some error in DB");
+     }
+});
+
+
+app.listen("8080", () => {
+    console.log("server is listening to port 8080");
+});
+
+//...............................................................
+
+
+//.......We can use it later if we need........
+// try {
+//     connection.query(q, [data], (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//     })
+// } catch (err) {
+//     console.log(err);
+// }
+
+// connection.end();
+//..............x-x-x...............................
+
+
+
+
 
 
 
